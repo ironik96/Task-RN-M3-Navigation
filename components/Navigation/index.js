ffd7523c1df53f68514ecc96be94e657950e9050
Ahmad AlAmiri <a.alamiri.96@gmail.com>
2022-06-15 17:25:42 +0300

    Add detail screen with routing

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ProductsList } from "../ProductsList";
+import { ProductDetails } from "../ProductDetail";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export default function RootNavigator() {
   return (
-    <Navigator>
+    <Navigator initialRouteName="Home">
       <Screen
         name="Home"
         component={ProductsList}
+        options={{ title: "Welcome" }}
+      />
+      <Screen
+        name="Product Detail"
+        component={ProductDetails}
         options={{ title: "Welcome" }}
       />
     </Navigator>
