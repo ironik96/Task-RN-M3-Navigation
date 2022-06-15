439c1c2c0ba056666daaa15205425064f814d857
Ahmad AlAmiri <a.alamiri.96@gmail.com>
2022-06-15 18:12:51 +0300

    Add cart icon to home screen

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { TouchableOpacity } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ProductsList } from "../ProductsList";
 import { ProductDetails } from "../ProductDetail";
+import { Cart } from "../Cart";
+import { Entypo } from "@expo/vector-icons";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -17,13 +20,21 @@ export default function RootNavigator() {
       <Screen
         name="Home"
         component={ProductsList}
-        options={{ title: "Products" }}
+        options={({ navigation }) => ({
+          title: "Products",
+          headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate("Cart")}>
+              <Entypo name="shopping-cart" size={24} color="white" />
+            </TouchableOpacity>
+          ),
+        })}
       />
       <Screen
         name="Product Detail"
         component={ProductDetails}
         options={{ title: "Welcome" }}
       />
+      <Screen name="Cart" component={Cart} options={{ title: "cart" }} />
     </Navigator>
   );
 }
